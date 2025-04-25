76a93f8fa2bb953667cc601ceb883756e8d4a92c
antho <antho@10.17.132.189>
2025-04-25 03:29:54 -0500

    Add Page, and Styling Throughout

--- src/java/business/Art.java
+++ src/java/business/Art.java
@@ -11,23 +11,32 @@ package business;
 public class Art {
     
     private int piece_id;
-    private int art_page_id;
     private String name;
     private double price;
+    private String size;
     private String medium;
     private String cover_image;
 
     public Art() {
     }
     
-    public Art(int piece_id, int art_page_id, String name, float price, String medium, String cover_image) {
+    public Art(int piece_id, String name, double price, String size, String medium, String cover_image) {
         this.piece_id = piece_id;
-        this.art_page_id = art_page_id;
         this.name = name;
         this.price = price;
+	this.size = size;
         this.medium = medium;
 	this.cover_image = cover_image;
     }
+
+    public String getSize() {
+	return size;
+    }
+
+    public void setSize(String size) {
+	this.size = size;
+    }
+    
 
     public int getPiece_id() {
         return piece_id;
@@ -35,14 +44,6 @@ public class Art {
 
     public void setPiece_id(int piece_id) {
         this.piece_id = piece_id;
-    }
-
-    public int getArt_page_id() {
-        return art_page_id;
-    }
-
-    public void setArt_page_id(int art_page_id) {
-        this.art_page_id = art_page_id;
     }
 
     public String getName() {
